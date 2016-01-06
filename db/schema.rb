f03711f454a70db34e44d408b26a8a6f5daea09e
Luis Mendoza <luis@mendoza.io>
2016-01-06 12:34:40 -0600

    Adds description to todo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106162152) do
+ActiveRecord::Schema.define(version: 20160106183338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,12 @@ ActiveRecord::Schema.define(version: 20160106162152) do
   end
 
   create_table "todos", force: :cascade do |t|
-    t.string   "title",      default: ""
-    t.boolean  "completed",  default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "title",       default: ""
+    t.boolean  "completed",   default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "list_id"
+    t.text     "description", default: ""
   end
 
 end
